6f1ed8fb45edf91e18c4b068da3829ee1a06f3ce
achrafezzaam <achraf.ezzaam@gmail.com>
2023-09-24 13:24:58 +0100

    Removing the match line

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,15 +5,11 @@ include stdlib
 file_line { 'file _identity':
   ensure  => present,
   path    => 'etc/ssh/ssh_config',
-  line    => 'IdentityFile ~/.ssh/school',
-  match   => '^IdentityFile',
-  replace => true,
+  line    => '\tIdentityFile ~/.ssh/school',
 }
 
 file_line { 'authentication':
   ensure  => present,
   path    => 'etc/ssh/ssh_config',
-  line    => 'PasswordAuthentication no',
-  match   => '^PasswordAuthentication',
-  replace => true,
+  line    => '\tPasswordAuthentication no',
 }
